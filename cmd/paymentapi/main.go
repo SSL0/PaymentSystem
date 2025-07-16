c5eca681f21499753876ec2dec192ebe5c6a2043
Log1c0 <log1c05678@gmail.com>
2025-07-16 16:45:35 +0700

    refactor: change main file name

--- cmd/paymentapi/main.go
+++ cmd/paymentapi/main.go
@@ -7,6 +7,14 @@ import (
 	"fmt"
 )
 
+// @title           Your API Title
+// @version         1.0
+// @description     This is a sample API.
+// @termsOfService  http://swagger.io/terms/
+// @contact.name    API Support
+// @contact.email   log1c05678@gmail.com
+// @host            localhost:8080
+// @BasePath        /api/v1
 func main() {
 	postgres, err := repository.NewPostgresdb()
 	if err != nil {
